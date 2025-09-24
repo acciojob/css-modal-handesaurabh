ecdc4f29a756f70adcb5ddc733541fa7b7b5d9bf
handesaurabh <handesaurabh2004@gmail.com>
2025-09-24 13:24:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+ const modal = document.getElementById("myModal");
+    const openBtn = document.getElementById("openModal");
+    const closeBtn = document.querySelector(".close-modal");
+
+    openBtn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+
+    closeBtn.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    }
